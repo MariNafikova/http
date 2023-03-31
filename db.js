30b08bcd53bcd382678199317140f031a7415e10
marina.nafikova <nafikova2708@mail.ru>
2023-03-31 19:54:10 +0300

    Выполнено домашнее задание #2

---
+++ db.js
@@ -0,0 +1,7 @@
+const user = {
+  id: 123,
+  username: "testuser",
+  password: "qwerty",
+};
+
+module.exports = Object.freeze(user);
--- server.js
+++ server.js
@@ -1,36 +1,86 @@
 const http = require("http");
 const fs = require("fs");
+const qs = require("querystring");
+const user = require("./db.js");
 
 const host = "localhost";
 const port = 8000;
 
 const requestListener = (req, res) => {
-  if (req.url === "/get" && req.method === "GET") {
-    function readFiles() {
-      return fs.readdirSync("./files", "utf-8");
-    }
-    try {
-      const result = readFiles();
+  let data = "";
+  req.on("data", (chunk) => {
+    data += chunk;
+  });
+
+  req.on("end", () => {
+    let dataObj = qs.parse(data);
+    if (req.url === "/auth" && req.method === "POST") {
+      console.log(dataObj);
+      if (
+        dataObj.username === user.username &&
+        dataObj.password === user.password
+      ) {
+        console.log("Authorization success");
+        res.writeHead(200, {
+          "Set-Cookie": [
+            "userId=" +
+              user.id +
+              "; expires=" +
+              new Date(new Date().getTime() + 30 * 60000).toUTCString(),
+            "authorized=" +
+              true +
+              "; expires=" +
+              new Date(new Date().getTime() + 30 * 60000).toUTCString(),
+          ],
+        });
+        res.statusCode = 200;
+        res.write(
+          JSON.stringify({
+            success: true,
+            id: user.id,
+            username: user.username,
+          })
+        );
+      } else {
+        console.log("Authorization fail");
+        res.statusCode = 401;
+        res.write(
+          JSON.stringify({
+            success: false,
+            result: "Неверный логин или пароль",
+          })
+        );
+      }
+    } else if (req.url === "/get" && req.method === "GET") {
+      function readFiles() {
+        return fs.readdirSync("./files", "utf-8");
+      }
+      try {
+        const result = readFiles();
+        res.statusCode = 200;
+        res.write("success: " + result);
+      } catch (err) {
+        res.statusCode = 500;
+        res.write("Internal server error");
+      }
+    } else if (req.url === "/post" && req.method === "POST") {
       res.statusCode = 200;
-      res.write("success: " + result);
-    } catch (err) {
-      res.statusCode = 500;
-      res.write("Internal server error");
+      res.write("success");
+    } else if (req.url === "/delete" && req.method === "DELETE") {
+      res.statusCode = 200;
+      res.write("success");
+    } else if (req.url === "/redirect" && req.method === "GET") {
+      res.statusCode = 200;
+      res.write(
+        "Данный ресурс теперь постоянно доступен по адресу /redirected"
+      );
+    } else {
+      res.statusCode = 405;
+      res.write("HTTP method not allowed");
     }
-  } else if (req.url === "/post" && req.method === "POST") {
-    res.statusCode = 200;
-    res.write("success");
-  } else if (req.url === "/delete" && req.method === "DELETE") {
-    res.statusCode = 200;
-    res.write("success");
-  } else if (req.url === "/redirect" && req.method === "GET") {
-    res.statusCode = 200;
-    res.write("Данный ресурс теперь постоянно доступен по адресу /redirected");
-  } else {
-    res.statusCode = 405;
-    res.write("HTTP method not allowed");
-  }
-  res.end();
+
+    res.end();
+  });
 };
 
 const server = http.createServer(requestListener);
